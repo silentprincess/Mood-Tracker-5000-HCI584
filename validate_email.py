345633465ac4d2702afad0b1abc8df246b28af9a
Sarah Hernandez <seh58765@gmail.com>
2022-06-21 20:04:03 -0500

    Worked on validating email addy code.

--- validate_email.py
+++ validate_email.py
@@ -28,25 +28,25 @@ def is_valid_email_address(emailAddress):
     #check the length of portion A
     a_length = len(emailAddress_a)
     if (a_length < 3 or a_length > 16):
-        return 5, "Email before @ must be between 3 and 16 characters, try again."
+        return False, "Email before @ must be between 3 and 16 characters, try again."
     
     #check if A is alphanumeric
     a_num = emailAddress_a.isalnum()
     if(a_num == 0):
-        return 6, "Email before @ is not alphanumeric, try again."
+        return False, "Email before @ is not alphanumeric, try again."
     
     #check the legnth of portion B
     b_length = len(emailAddress_b)
     if (b_length < 2 or b_length > 8):
-        return 7, "Email after @ must be between 2 and 8 characters, try again."
+        return False, "Email after @ must be between 2 and 8 characters, try again."
     
     #check if B is alphanumeric
     b_num = emailAddress_b.isalnum()
     if (b_num == 0):
-        return 8, "Email after @ is not alphanumeric, try again."
+        return False, "Email after @ is not alphanumeric, try again."
     
     #check if C is com, edu, org, gov
     if emailAddress_c not in ["com", "edu", "org", "gov"]:
-        return 9, "Email does not have valid domain. Try again."
+        return False, "Email does not have valid domain. Try again."
     
-    return None, "Seems Legit."
+    return True, "Seems Legit."
